468127973861511f9a25dfa2dffe5bb653110525
tesiaka <teddysiakaogola@gmail.com>
2025-01-27 07:54:12 +0300

    implement function to fetch riddles from the server

--- js/GameMode.js
+++ js/GameMode.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded",()=>{
-let classicBtn=document.getElementById("classic")
-classicBtn.onclick=()=>{
-    window.location.href="/playsection?gamemode=classic";
-}
+// let classicBtn=document.getElementById("classic")
+// classicBtn.onclick=()=>{
+//     window.location.href="/classic";
+// }
 
-let timeBtn=document.getElementById("time")
-timeBtn.onclick=()=>{
-    window.location.href="/playsection?gamemode=timeattack"
-}
+// let timeBtn=document.getElementById("time")
+// timeBtn.onclick=()=>{
+//     window.location.href="/playsection?gamemode=timeattack"
+// }
 
-let survivalBtn=document.getElementById("survival")
-survivalBtn.onclick=()=>{
-    window.location.href="/playsection?gamemode=survival"
-}
+// let survivalBtn=document.getElementById("survival")
+// survivalBtn.onclick=()=>{
+//     window.location.href="/playsection?gamemode=survival"
+// }
 
 let difficultyBtn=document.getElementById("difficulty")
 difficultyBtn.onclick=()=>{
